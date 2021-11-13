19dc3ad9d865a4b2ea133962798af25d47fde9e5
JeanElossy <jean.elossy@yahoo.com>
2021-11-13 22:27:48 +0100

    Initialisation du projet

--- src/components/Connexion.jsx
+++ src/components/Connexion.jsx
@@ -8,31 +8,76 @@ const Connexion = ( { className }) => {
         <div className={className}>
             <button 
                 type="button" 
-                className="btn btn-primary"
+                className="btn btn-outline-primary"
                 onClick={(e) => setToggle(!toggle)}
                 >
                     Connexion
             </button>
             
             <div className={ toggle ? "show modal fade " : "overlay" } id="overlay">
-                    <div className="modal-content d-flex">
-                            <h5 className="modal-title" id="connexion">Modal title</h5>
-                            <div>
-                                <button 
-                                    type="button" 
-                                    className="btn-close"
-                                    data-bs-dismiss="modal" 
-                                    aria-label="Close"
-                                    onClick={(e) => setToggle(!toggle)}
-                                    >
-
-                                </button>
-                            </div>
-                           
-                        
+                <div className="modal-content">
+                    <div>
+                        <h6 className="modal-title mt-3 ms-4" id="connexion">Connexion</h6>
+                        <div>
+                            <button 
+                                type="button" 
+                                className="btn-close"
+                                onClick={(e) => setToggle(!toggle)}
+                            >
+                            </button>
+                        </div>
                     </div>
-                
-            </div>
+                    <hr 
+                        style={{width:"90%", transform: "translate(5%)"}}
+                        className="mt-2"
+                    />
+                    <div className="con-reseaux">
+                        <div className="ms-4">Vous n'avez pas de compte ? <span><a href="/" className="span">Inscrivez-vous</a></span></div>
+                        <div className="d-flex justify-content-center gap-3 mt-3">
+                            <button className="btn-reseau">
+                                <span></span> <strong>Connexon avec Google</strong>
+                            </button>
+                            <button className="btn-reseau">
+                                <span></span> <strong>Connexon avec Facebook</strong>
+                            </button>
+                        </div>
+                    </div> 
+                    <div className="text-center">
+                        <span className="ou">Ou</span>
+                    </div>
+                    <div className="mt-4 ">
+                        <form action="/" className="form-group ms-3">
+                            <input 
+                                type="Email" 
+                                name="email"
+                                id="email"
+                                placeholder="Adresse email"
+                                className="form-control"
+                                style={{width: "415px"}}
+                            />
+                            <input 
+                                type="password" 
+                                name="password"
+                                id="password"
+                                placeholder="Mot de passe"
+                                className="form-control mt-3"
+                                style={{width: "415px"}}
+                            />
+                            <input 
+                                type="submit" 
+                                value="Se connecter"
+                                className="btn text-white mt-3"
+                                style={{width: "415px"}}
+                            />
+                        </form>
+                        <div className="m-3">
+                            <a href="/" className="forget">
+                                Mot de passe oublié 
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div> 
         </div>
     )
 }
--- src/components/Inscription.jsx
+++ src/components/Inscription.jsx
@@ -15,20 +15,102 @@ const Inscritpion = ( { className }) => {
             </button>
             
             <div className={ toggle ? "show modal fade " : "overlay" } id="overlay">
-                    <div className="modal-content d-flex">
-                            <h5 className="modal-title" id="Inscritpion">Modal title</h5>
+                    <div className="modal-content">
+                        <div>
+                            <h5 className="modal-title mt-3 ms-4 text-dark" id="Inscritpion">Inscription</h5>
                             <div>
                                 <button 
                                     type="button" 
                                     className="btn-close"
-                                    data-bs-dismiss="modal" 
-                                    aria-label="Close"
                                     onClick={(e) => setToggle(!toggle)}
                                     >
-
                                 </button>
                             </div>
+                        </div>
+                        <hr 
+                            style={{width:"90%", transform: "translate(5%)"}}
+                            className="mt-2"
+                        />
+                        <div className="ins-reseaux">
+                            <div className="ms-4">Vous avez un compte ? <span><a href="/" className="span">Connectez-vous</a></span></div>
+                            <div className="d-flex justify-content-center gap-3 mt-3">
+                                <button className="btn-reseau">
+                                    <span></span> <strong>Connexon avec Google</strong>
+                                </button>
+                                <button className="btn-reseau">
+                                    <span></span> <strong>Connexon avec Facebook</strong>
+                                </button>
+                            </div>
+                        </div>
+                        <div className="text-center">
+                        <span className="ou">Ou</span>
                     </div>
+                    <div className="mt-4 ">
+                        <form action="/" className="form-group ms-3">
+                            <input 
+                                type="text" 
+                                name="nom"
+                                id="nom"
+                                placeholder="Nom"
+                                className="form-control"
+                                style={{width: "415px"}}
+                            />
+                            <input 
+                                type="text" 
+                                name="prenom"
+                                id="prenom"
+                                placeholder="Prenom(s)"
+                                className="form-control mt-3"
+                                style={{width: "415px"}}
+                            />
+                            <input 
+                                type="Email" 
+                                name="email"
+                                id="email"
+                                placeholder="Email"
+                                className="form-control mt-3"
+                                style={{width: "415px"}}
+                            />
+                            <input 
+                                type="password" 
+                                name="password"
+                                id="password"
+                                placeholder="Mot de passe"
+                                className="form-control mt-3"
+                                style={{width: "415px"}}
+                            />
+                            
+                            <select 
+                                name="pays" 
+                                id="pays"
+                                className="select-control mt-3"
+                                style={{width: "415px", border: "1px solid rgba(165, 164, 164, 0.600)", padding: ".4rem 0", borderRadius: "5px"}}
+                            >
+                                <option value="Pays">Pays</option>
+                                <option value="Ci">Cote d'Ivoire</option>
+                                <option value="France">France</option>
+                            </select>
+
+                            <select 
+                                name="ville" 
+                                id="ville"
+                                className="select-control mt-3"
+                                style={{width: "415px", border: "1px solid rgba(165, 164, 164, 0.600)", padding: ".4rem 0", borderRadius: "5px"}}
+                            >
+                                <option value="Ville">Ville</option>
+                                <option value="Abidjan">Abidjan</option>
+                                <option value="Bouake">Bouake</option>
+                            </select>
+                            
+                            <input 
+                                type="submit" 
+                                value="S'inscrire"
+                                className="btn btn-primary mt-3"
+                                style={{width: "415px"}}
+                            />
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     )
